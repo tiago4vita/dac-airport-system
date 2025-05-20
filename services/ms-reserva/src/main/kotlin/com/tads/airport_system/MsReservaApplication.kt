240f76e238402794ab9aef99349743023c6c8968
andrejpg <andremoraisbrito556@gmail.com>
2025-05-20 13:23:03 -0300

    feat(R07): inicializa projeto do MS de Reservas

---
+++ services/ms-reserva/src/main/kotlin/com/tads/airport_system/MsReservaApplication.kt
@@ -0,0 +1,11 @@
+package com.tads.airport_system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsReservaApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsReservaApplication>(*args)
+}
---
+++ services/ms-reserva/src/test/kotlin/com/tads/airport_system/MsReservaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tads.airport_system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsReservaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
