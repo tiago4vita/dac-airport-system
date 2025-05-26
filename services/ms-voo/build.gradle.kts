78ac0b2d8f57e738e716bdab30ce6ca0ccbebc30
CJarlos <carlosrochacr11@outlook.com>
2025-05-25 22:22:49 -0300

    fix(ms-voo): Ajustes no microservico de voo, implantação de classes DTO, ajustes no repository, service e controller.

--- services/ms-voo/build.gradle.kts
+++ services/ms-voo/build.gradle.kts
@@ -21,11 +21,13 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-amqp")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("org.springframework.amqp:spring-rabbit-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	
 }
 
 kotlin {
