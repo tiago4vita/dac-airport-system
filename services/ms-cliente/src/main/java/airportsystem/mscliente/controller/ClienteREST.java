cd781695e5648998828109c2b2e35981368b75ff
tiago4vita <tiago.parejavita4@gmail.com>
2025-06-03 13:55:21 -0300

    refactor(ms-cliente): Comenta ClienteController pois não sera usado

--- services/ms-cliente/src/main/java/airportsystem/mscliente/controller/ClienteREST.java
+++ services/ms-cliente/src/main/java/airportsystem/mscliente/controller/ClienteREST.java
@@ -1,36 +1,36 @@
-package airportsystem.mscliente.controller;
+// package airportsystem.mscliente.controller;
 
-import airportsystem.mscliente.model.Cliente;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+// import airportsystem.mscliente.model.Cliente;
+// import com.fasterxml.jackson.core.JsonProcessingException;
+// import com.fasterxml.jackson.databind.ObjectMapper;
+// import org.springframework.amqp.rabbit.core.RabbitTemplate;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.CrossOrigin;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
-@RestController("/api")
-public class ClienteREST {
+// @CrossOrigin
+// @RestController("/api")
+// public class ClienteREST {
 
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
+//     @Autowired
+//     private RabbitTemplate rabbitTemplate;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+//     @Autowired
+//     private ObjectMapper objectMapper;
 
-    @PostMapping("/clientes")
-    ResponseEntity<?> enfileirarPessoa(@RequestBody Cliente p)
-                throws JsonProcessingException {
+//     @PostMapping("/clientes")
+//     ResponseEntity<?> enfileirarPessoa(@RequestBody Cliente p)
+//                 throws JsonProcessingException {
 
-        var json = objectMapper.writeValueAsString(p);
+//         var json = objectMapper.writeValueAsString(p);
 
-        rabbitTemplate.convertAndSend("retorno", json);
+//         rabbitTemplate.convertAndSend("retorno", json);
 
-        return new ResponseEntity<>("Enfileirado: " + json, HttpStatus.OK);
+//         return new ResponseEntity<>("Enfileirado: " + json, HttpStatus.OK);
 
-    }
-}
+//     }
+// }
