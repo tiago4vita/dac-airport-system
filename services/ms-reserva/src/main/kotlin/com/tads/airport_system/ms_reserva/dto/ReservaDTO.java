7394a343acef5e2ec8fb2694fcffa2b2410b055f
andrejpg <andremoraisbrito556@gmail.com>
2025-06-05 16:04:40 -0300

    refactor(R07): arruma a classe reserva para java e atualiza dto corretamente

---
+++ services/ms-reserva/src/main/kotlin/com/tads/airport_system/ms_reserva/dto/ReservaDTO.java
@@ -0,0 +1,57 @@
+package com.tads.airport_system.ms_reserva.dto
+
+import com.tads.airport_system.ms_reserva.model.Reserva
+import java.time.LocalDateTime
+
+import java.util.Objects;
+
+public class ReservaDTO {
+
+    public ReservaDTO() {}
+
+    public ReservaDTO (String id, String vooId, LocalDateTime dataHoraRes, String estado) {
+        this.id = id;
+        this.vooId = vooId;
+        this.dataHoraRes = dataHoraRes;
+        this.estado = estado
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getVooId(){
+        return vooId;
+    }
+
+    public LocalDateTime getDataHoraRes() {
+        return dataHoraRes
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setID(String id) {
+        this.id = id;
+    }
+
+    public void setVooID(String vooId) {
+        this.vooId = vooId;
+    }
+
+    public void setDataHoraRes(LocalDateTime dataHoraRes) {
+        this.dataHoraRes = dataHoraRes;
+    }
+
+    @Override
+    public String toString() {
+        return "ReservaDTO{" +
+                "id='" + id + '\'' +
+                ", vooId='" + vooId + '\'' +
+                ", dataHoraRes='" + dataHoraRes + '\'' +
+                ", estado=" + estado +
+                '}';
+    }
+    
+}
---
+++ services/ms-reserva/src/main/kotlin/com/tads/airport_system/ms_reserva/model/Reserva.java
@@ -0,0 +1,64 @@
+package airportsystem.msreserva.model;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "reservas")
+public class Reserva {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(unique = true)
+    private String id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(unique = true)
+    private String vooId;
+
+    @Column(name = "dataHoraRes", nullable = false)
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private LocalDateTime dataHoraRes = LocalDateTime.now();
+    
+    @Column(nullable = false)
+    private String estado;
+
+    public Cliente(String cpf, String nome, String email, EnderecoDTO endereco) {
+        this.id = id;
+        this.vooId = vooId;
+        this.email = email;
+        this.estado = estado;
+        this.dataHoraRes = LocalDateTime.now();
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getVooId(){
+        return vooId;
+    }
+
+    public LocalDateTime getDataHoraRes() {
+        return dataHoraRes
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setID(String id) {
+        this.id = id;
+    }
+
+    public void setVooID(String vooId) {
+        this.vooId = vooId;
+    }
+
+    public void setDataHoraRes(LocalDateTime dataHoraRes) {
+        this.dataHoraRes = dataHoraRes;
+    }
+}
