19879dac87a96855ba1276f07e52c596f66c6df7
tiago4vita <tiago.parejavita4@gmail.com>
2025-06-12 14:42:30 -0300

    refactor(ms-auth): Refatora em Java e cria consumer CriarUsuario (funcionando)

---
+++ services/ms-auth/src/main/java/airportsystem/msauth/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package airportsystem.msauth.repository;
+
+import airportsystem.msauth.model.Usuario;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends MongoRepository<Usuario, String> {
+
+    boolean existsByLogin(String login);
+    boolean existsByCodigo(String codigo);
+
+}
